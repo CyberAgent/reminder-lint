93c44aa48243c2dfd6065fce83de2bd10d9d47cc
kqito <29191111+kqito@users.noreply.github.com>
2024-05-19 14:40:30 +0900

    split into some workspaces

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+
 use config::Config as ConfigBuilder;
 use serde::Deserialize;
 
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,24 +1,39 @@
-mod remind;
 mod config;
+mod remind;
 
-use std::error::Error;
 use crate::config::load_config;
 use crate::remind::list_reminders;
+use std::error::Error;
 
 const REMIND_IGNORE_CONFIG_FILE: &str = ".remindignore";
 const REMIND_CONFIG_FILE: &str = "remind.yaml";
 
-fn main() -> Result<(), Box<dyn Error>> {
+pub struct Reminders {
+    pub expired: Vec<remind::Remind>,
+    pub upcoming: Vec<remind::Remind>,
+}
+
+pub fn reminders() -> Result<Reminders, Box<dyn Error>> {
     let conf = load_config(REMIND_CONFIG_FILE)?;
     let reminders = list_reminders(
         &conf.comment_regex,
         &conf.datetime_format,
         REMIND_IGNORE_CONFIG_FILE,
-        &conf.search_directory)?;
+        &conf.search_directory,
+    )?;
+
+    let mut expired = Vec::new();
+    let mut upcoming = Vec::new();
+
     for remind in reminders {
         if remind.datetime < chrono::Local::now().timestamp() {
-            println!("{}:{} {}", remind.position.file, remind.position.line, remind.message);
+            expired.push(remind);
+        } else {
+            upcoming.push(remind);
         }
     }
-    Ok(())
+
+    let reminders = Reminders { expired, upcoming };
+
+    Ok(reminders)
 }
