689d3a0183e558a416a88e4d51c981ae88819a8a
kqito <29191111+kqito@users.noreply.github.com>
2024-06-19 11:45:18 +0900

    chore: update testdata

--- testdata/pkg/main.go
+++ testdata/pkg/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    // remind: 2024/05/03 @arabian9ts test
-    fmt.Println("Hello, World!")
+	// @arabian9ts remind: 2024/05/03 test
+	fmt.Println("Hello, World!")
+	// remind: 2024/05/03 @arabian9ts test
+	fmt.Println("Hello, World!")
 }
