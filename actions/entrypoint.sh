6bd4960b6125a70900795f547ce78065629493c4
Taku Maeda <nok14.tm59@gmail.com>
2024-10-18 12:57:10 +0900

    action: fixed custom action for multiline output case (#43)

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -l
 
 /usr/local/bin/reminder-lint/cli "$@" > /tmp/reminder-lint.log 2>&1
-echo "stdout=$(cat /tmp/reminder-lint.log)" >> $GITHUB_OUTPUT
+
+echo "stdout<<EOF" >> $GITHUB_OUTPUT
+cat /tmp/reminder-lint.log >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
