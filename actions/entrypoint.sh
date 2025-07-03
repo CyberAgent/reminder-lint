2a867b065b0b020088b7ed4c855847223f05efd4
kqito <29191111+kqito@users.noreply.github.com>
2025-07-03 10:54:53 +0900

    feat: add image_tag option (#64)

--- actions/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-/usr/local/bin/reminder-lint/cli "$@" > /tmp/reminder-lint.log 2>&1
-
-echo "stdout<<EOF" >> $GITHUB_OUTPUT
-cat /tmp/reminder-lint.log >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
